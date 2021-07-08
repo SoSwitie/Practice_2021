f0d9cadc9967f1e82bd27e98411ad12e1629b283
Natalia Karmazina <86661251+SoSwitie@users.noreply.github.com>
2021-07-08 11:52:04 +0300

    Update task_0.py

--- Task 0/task_0.py
+++ Task 0/task_0.py
@@ -1,1 +1,15 @@
+# Варіант 1
+import random
+arr = [random.randint(-100,100) for i in range(30)]
+MaxNumb=max(arr)
+arr.index(MaxNumb)
+print(arr)
+print("Max number is " + str(MaxNumb))
+print("This number is in " + str(arr.index(MaxNumb)+1)+"th place")
+for i in range(29):
+    if i%2 ==0:
+        odds =[ n for n in arr if n%2]
+odds.sort(reverse = True)
+print(odds)
+
 
